ca1ec0ef7ebc505a0abbf2f9fdb0f70cae47cb9f
nollenr <nollenr@gmail.com>
2023-07-09 18:05:52 -0700

    added virtual network and and id to output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "join_string" {
   description = "the CockroachDB private IP join string"
   value = local.join_string
 }
+
+output "virtual_network_name" {
+  description = "Virtual Network Name"
+  value = azurerm_virtual_network.vm01.name
+}
+
+output "virtual_network_id" {
+  description = "virtual Network ID"
+  value = azurerm_virtual_network.vm01.id
+}
