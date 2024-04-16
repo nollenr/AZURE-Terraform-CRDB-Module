b56a4054a3016211c7f0a34cfceeeb1d0d27e448
nollenr <nollenr@gmail.com>
2024-04-16 08:39:42 -0700

    changed instance disk resize

--- instance.tf
+++ instance.tf
@@ -88,7 +88,11 @@ if [ "${var.crdb_resize_homelv}" = "yes" ]
 then 
   echo "Attempting to resize /dev/mapper/rootvg-homelv with any space available on the physical volume"
   echo "Resize the Linux LVM"
-  growpart /dev/sda 2
+  if growpart /dev/sda 2; then
+    echo "sda 2 has been resized"
+  else
+    echo "sda2 did not need to be resized"
+  fi
   echo "Capture the free space on the device in GB.  The awk command is capturing only the integer portion of the output"
   ds=`pvs -o name,free --units g --noheadings | awk '{printf "%dG\n", \$2}'`
   echo "Resizing the logical volume by $ds"
