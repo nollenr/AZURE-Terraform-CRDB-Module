37d7267f9a4f72340da4b67880e9b3307bf22f93
nollenr <nollenr@gmail.com>
2024-09-03 07:52:53 -0700

    constraining the azure terraform provider to a 3.x version until 4+ provider version is changed.

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      version = ">=3.63.0"
+      version = "~> 3.63"  # temporariliy constraining the version to 3.x until problems with 4.x are fixed
     }
   }
   required_version = ">=1.5.2"
