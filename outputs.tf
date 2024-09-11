0d812157221a22d8f5a338309f3df361cf42bf8f
nollenr <nollenr@gmail.com>
2024-09-11 08:30:15 -0700

    changed output.tf to supply needed values for PCR Target.   Added several terraform.tfvars examples in a subfolder

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,31 @@ output "virtual_network_id" {
   description = "virtual Network ID"
   value = azurerm_virtual_network.vm01.id
 }
+
+output "tls_private_key" {
+  description = "tls_private_key.crdb_ca_keys.private_key_pem -> ca.key / TLS Private Key PEM"
+  value = local.tls_private_key
+  sensitive = true
+}
+
+output "tls_public_key" {
+  description = "tls_private_key.crdb_ca_keys.public_key_pem -> ca.pub / TLS Public Key PEM"
+  value = local.tls_public_key
+}
+
+output "tls_cert" {
+  description = "tls_self_signed_cert.crdb_ca_cert.cert_pem -> ca.crt / TLS Cert PEM  /  Duplicate of tls_cert for better naming"
+  value = local.tls_cert
+}
+
+output "tls_user_cert" {
+  description = "tls_locally_signed_cert.user_cert.cert_pem -> client.name.crt"
+  value = local.tls_user_cert
+}
+
+output "tls_user_key" {
+  description = "tls_private_key.client_keys.private_key_pem -> client.name.key"
+  value = local.tls_user_key
+  sensitive = true
+}
+
